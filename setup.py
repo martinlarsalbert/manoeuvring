4f8ba398a2348bcc54b8ae28adbb287bcaba04a4
Martin Alexandersson <marale@kth.se>
2020-08-28 14:15:06 +0200

    Added stuff created by the cookiecutter template.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The work conducted for a paper about system identification of ship manoeuvring is stored here.',
+    author='Martin Alexanderson',
+    license='MIT',
+)
